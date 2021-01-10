1f6d533300fd64a739930fd42f0572328be48469
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2021-01-10 21:53:35 +0000

    Changed complete type of execute_command to Option<Value>, as anything _can_ be returned

--- src/server_tests.rs
+++ src/server_tests.rs
@@ -191,7 +191,7 @@ impl LanguageServerHandling for TestsLanguageServer {
     fn rename(&mut self, _: RenameParams, completable: LSCompletable<WorkspaceEdit>) {
         completable.complete(Err(Self::error_not_available(())));
     }
-    fn execute_command(&mut self, _: ExecuteCommandParams, completable: LSCompletable<WorkspaceEdit>) {
+    fn execute_command(&mut self, _: ExecuteCommandParams, completable: LSCompletable<Option<Value>>) {
         completable.complete(Err(Self::error_not_available(())));
     }
 }
